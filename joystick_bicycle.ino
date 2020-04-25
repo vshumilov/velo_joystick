8e0855809a46f50aae319a78ffe10ef8eddd7f11
vshumilov <vladimirshumilov@mail.ru>
2020-04-26 01:14:13 +0300

    buttons added
    Now 7 buttons on steering

--- joystick_bicycle.ino
+++ joystick_bicycle.ino
@@ -6,15 +6,20 @@
 #define RPM 2
 #define BTN_1 7
 #define BTN_2 8
+#define BTN_3 9
+#define BTN_4 10
+#define BTN_5 11
+#define BTN_6 12
+#define BTN_7 13
 
 // Create Joystick
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
-  JOYSTICK_TYPE_JOYSTICK, 2, 0,
+  JOYSTICK_TYPE_JOYSTICK, 7, 0,
   false, false, false, false, false, false,
   false, true, false, false, true);
 
 unsigned int min_speed = 0;  // minimal speed to display on scale, km/h
-unsigned int max_speed = 90; // maximum speed to display on scale, km/h
+unsigned int max_speed = 80; // maximum speed to display on scale, km/h
 
 // Set to true to test "Auto Send" mode or false to test "Manual Send" mode.
 //const bool testAutoSendMode = true;
@@ -45,11 +50,16 @@ void setup() {
   pinMode (RPM, INPUT_PULLUP);
   pinMode (BTN_1, INPUT_PULLUP);
   pinMode (BTN_2, INPUT_PULLUP);
+  pinMode (BTN_3, INPUT_PULLUP);
+  pinMode (BTN_4, INPUT_PULLUP);
+  pinMode (BTN_5, INPUT_PULLUP);
+  pinMode (BTN_6, INPUT_PULLUP);
+  pinMode (BTN_7, INPUT_PULLUP);
   
   // Initialize Joystick Library
   Joystick.begin(testAutoSendMode);
   
-  Serial.begin(9600);
+  //Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(RPM), sens, FALLING);  // hall sensor interrupt  
 }
 
@@ -67,12 +77,18 @@ void loop() {
     }
   }
 
-  Joystick.setSteering(map(analogRead(STEERING), 0, 671, 0, 1023));
+  Joystick.setSteering(map(analogRead(STEERING), 40, 685, 0, 1023));
   Joystick.setThrottle(pos);
   Joystick.setButton(0, !digitalRead(BTN_1));
   Joystick.setButton(1, !digitalRead(BTN_2));
+  Joystick.setButton(2, !digitalRead(BTN_3));
+  Joystick.setButton(3, !digitalRead(BTN_4));
+  Joystick.setButton(4, !digitalRead(BTN_5));
+  Joystick.setButton(5, !digitalRead(BTN_6));
+  Joystick.setButton(6, !digitalRead(BTN_7));
   Joystick.sendState();
   //Serial.println("Steering " + String(map(analogRead(STEERING), 0, 671, 0, 1023)) + ", RPM " + String(digitalRead(RPM)) + ", Speed " + String(curSpeed) + ", pos " + String(pos));
+  //Serial.println("Steering " + String(map(analogRead(STEERING), 40, 685, 0, 1023)) + ", Speed " + String(curSpeed) + ", pos " + String(pos));
   //Serial.println("Speed " + String(curSpeed) + ", pos " + String(pos));
-  delay(100);
+  delay(50);
 }
